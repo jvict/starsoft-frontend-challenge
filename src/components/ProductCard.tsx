9366266be02949f880c83e0e6029db3481a70a02
clickaqui-dig <clickaquidigital@outlook.com>
2025-03-28 23:47:46 -0300

    feat: Add function control visible Overlay

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,7 +1,8 @@
 "use client";
-import { useDispatch } from 'react-redux';
-import { addToCart } from '../store/slices/cartSlie';
+import { useDispatch } from "react-redux";
+import { addToCart } from "../store/slices/cartSlie";
 import styles from "../styles/ProductCard.module.scss";
+import Ellipse from "../utils/images/Ellipse 770.svg";
 
 interface ProductProps {
   product: {
@@ -9,22 +10,32 @@ interface ProductProps {
     name: string;
     price: number;
     image: string;
-    quantity:number
+    quantity: number;
+    description : string;
   };
+  onAddToCart: () => void; // Nova prop para controlar o overlay
 }
 
-
-const ProductCard = ({ product }: any) => {
+const ProductCard: React.FC<ProductProps> = ({ product, onAddToCart }) => {
   const dispatch = useDispatch();
+
+  const handleAddToCart = () => {
+    dispatch(addToCart(product)); // Adiciona o produto ao carrinho
+    onAddToCart(); // Controla a exibição do overlay
+  };
+
   return (
     <div className={styles.card}>
       <img src={product.image} alt={product.name} className={styles.productImage} />
       <div className={styles.productDetails}>
         <h3 className={styles.productName}>{product.name}</h3>
         <p className={styles.productPrice}>
+          <img src={Ellipse.src} alt="Carrinho" className={styles.ellipseIcon} />
           <span className={styles.priceValue}>{product.price} ETH</span>
         </p>
-        <button className={styles.buyButton} onClick={() => dispatch(addToCart(product))}>Comprar</button>
+        <button className={styles.buyButton} onClick={handleAddToCart}>
+          COMPRAR
+        </button>
       </div>
     </div>
   );
