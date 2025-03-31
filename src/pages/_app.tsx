5ca26434b9d8f8ba5889c18ba86bc49fc11bd8e6
clickaqui-dig <clickaquidigital@outlook.com>
2025-03-31 01:15:41 -0300

    feat: Use layout in _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { Provider } from 'react-redux';
 import { QueryClientProvider, QueryClient } from 'react-query';
 import { store } from '../store';
 import '../styles/global.scss';
+import Layout from '@/components/Layout';
 
 const queryClient = new QueryClient();
 
@@ -10,7 +11,9 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
       <QueryClientProvider client={queryClient}>
-        <Component {...pageProps} />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
       </QueryClientProvider>
     </Provider>
   );
