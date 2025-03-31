5afc57e5b79d56fa9dbcf88314ed11dd6e517df1
clickaqui-dig <clickaquidigital@outlook.com>
2025-03-31 01:14:10 -0300

    feat: Add Layout in pages with navbar and footer

---
+++ src/components/Layout.tsx
@@ -0,0 +1,19 @@
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+import styles from "../styles/Layout.module.scss";
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout = ({ children }: LayoutProps) => {
+  return (
+    <div className={styles.layout}>
+      <Navbar />
+      <main className={styles.main}>{children}</main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
